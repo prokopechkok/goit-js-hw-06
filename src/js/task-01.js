be04c61c17a9a799f046c306b43c6114f97784e4
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-10-04 21:04:43 +0300

    task1 done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,1 +1,8 @@
-console.log("Hello");
+const categories = document.querySelector("#categories");
+console.log(`Number of categories: ${categories.children.length}`);
+
+[...categories.children].forEach((li) => {
+  console.log(`Category: ${li.firstElementChild.textContent}`);
+  const innerUl = li.querySelector("ul");
+  console.log(`Elements: ${innerUl.children.length}`);
+});
