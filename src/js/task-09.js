484552f0134ea4cb1726082594df2b7a49f54a56
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-10-06 22:12:31 +0300

    task9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", onClick);
+
+function onClick(event) {
+  const bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+  span.textContent = bgColor;
+}
